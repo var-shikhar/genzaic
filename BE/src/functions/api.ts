3a057e8301860f162670cd7b094493b9c4961f6b
Shikhar Varshney <shikharvarshney10@gmail.com>
2026-06-21 18:30:02 +0530

    fix: old code removed

--- BE/src/functions/api.ts
+++
@@ -1,9 +0,0 @@
-import serverless from 'serverless-http';
-import createApp from '../app';
-import { env } from '../config/environment';
-
-// Initialize the app
-const app = createApp();
-
-// Export the handler for Netlify Functions
-export const handler = serverless(app);
